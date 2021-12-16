5d98967d6f103315b986861ea1069e10b17988e6
JustynBougeno <93269509+JustynBougeno@users.noreply.github.com>
2021-12-16 16:43:03 -0600

    Passed last test

--- TechJobsTests/JobTests.cs
+++ TechJobsTests/JobTests.cs
@@ -12,6 +12,7 @@ namespace TechJobsTests
         Job job3;
         Job job4;
         Job job5;
+        //object Id = null;
         //creates jobs object method.
         [TestInitialize]
         public void CreateJobObjects()
@@ -21,7 +22,7 @@ namespace TechJobsTests
             // this will need to go in the test.
             job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality Control"), new CoreCompetency("Persistence")); 
             job4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality Control"), new CoreCompetency("Persistence"));
-            job5 = new Job("Ice cream tester", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Tasting ability"));
+            job5 = new Job("Ice cream tester", new Employer(""), new Location("Home"), new PositionType(""), new CoreCompetency("Tasting ability"));
 
         }
 
@@ -68,22 +69,29 @@ namespace TechJobsTests
         [TestMethod]
         public void TestToStringOutput()
         {
-           string acutual = job4.ToString();
-           string expected = "\r\nName: Product tester\r\nEmployer: ACME\r\nLocation: Desert"
-                + "\r\nPosition: Quality Control\r\nCompetency: Persistence\r\n";
-            Assert.AreEqual(expected, acutual);
+           //string acutual = job4.ToString();
+           string expected = $"\r\nID: {job4.Id}\r\nName: {job4.Name}\r\nEmployer: {job4.EmployerName.Value}\r\n" +
+                $"Location: {job4.EmployerLocation.Value}\r\nPosition: {job4.JobType.Value}\r\nCompetency: {job4.JobCoreCompetency.Value}\r\n";
+            Assert.AreEqual(expected, job4.ToString());
         }
 
         [TestMethod]
         public void TestEmptySpots()
         {
-            string acutual = job5.ToString();
 
-            string expected = "\r\nName: Ice cream tester\r\nEmployer: Data not available\r\nLocation: Home"
-                + "\r\nPosition: UX\r\nCompetency: Tasting ability\r\n";
+
+
+            //string expected = $"\r\nID: 5\r\nName: Ice cream tester\r\nEmployer: Data not available\r\nLocation: Home"
+            //    + "\r\nPosition: UX\r\nCompetency: Tasting ability\r\n";
+            //Assert.AreEqual(expected, acutual);
+            string acutual = job5.ToString();
+            //job5.EmployerName.Value = "";
+            //job5.JobType.Value = "";
+            string expected = $"\r\nID: {job5.Id}\r\nName: {job5.Name}\r\nEmployer: Data not available\r\nLocation: {job5.EmployerLocation.Value}\r\nPosition: Data not available\r\nCompetency: {job5.JobCoreCompetency.Value}\r\n";
             Assert.AreEqual(expected, acutual);
         }
     }
+   
 
 }
 
